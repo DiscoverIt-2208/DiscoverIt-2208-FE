6dde5d4f3b208cbc480a60a9c48364e8ee868b56
Dani Bagley <bagley.a.danielle@gmail.com>
2023-02-05 15:48:06 -0700

    Add logo

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import SearchBar from '../SearchBar/SearchBar'
-import Logo from '../../assets/images/logo.png'
-import Loader from 'react-loaders'
-import './Home.css'
-
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import Logo from "../assets/DiscoverItLogo.svg";
+import Loader from "react-loaders";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <>
-            <div className='container home-page'>
-                <img src={Logo} alt='logo' />
-                <SearchBar />
-                <div className='search-results'>
-                    maybe we can display the cards here?
-                </div>
-            </div>
-            <Loader type='ball-scale-multiple' />
-        </>
-    )
-}
+  return (
+    <>
+      <div className="container home-page">
+        <img src={Logo} alt="logo" />
+        <SearchBar />
+        <div className="search-results">
+          maybe we can display the cards here?
+        </div>
+      </div>
+      <Loader type="ball-scale-multiple" />
+    </>
+  );
+};
 
-export default Home
+export default Home;
