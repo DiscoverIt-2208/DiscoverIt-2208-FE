8284dd46fb07eb108c15683c3befe2b538bb3e0a
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-02-06 19:26:49 -0600

    Finished SPLASHPAGE

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from '../Header/Header'
 import Home from "../Home/Home";
 import "./App.scss";
 
 const App = () => {
   return (
     <>
-      <Header />
       <div className="app-container">
         <Home />
       </div>
