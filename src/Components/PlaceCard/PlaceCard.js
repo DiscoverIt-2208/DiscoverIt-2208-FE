ae3d2fd140f8c3aad6c2e81b3fb64b1947168d1c
Jenn Yacoubian <jyacoubian@gmail.com>
2023-02-08 08:57:23 -0700

    Add alt tag to card images

--- src/Components/PlaceCard/PlaceCard.js
+++ src/Components/PlaceCard/PlaceCard.js
@@ -6,7 +6,7 @@ const PlaceCard = ({places}) => {
   places.map(place => {
     return (
       <div className='card-container'>
-        <img src={place.image}/>
+        <img src={place.image} alt={`image of ${place.name}`}/>
         <h3>{place.name}</h3>
       </div>
     )
