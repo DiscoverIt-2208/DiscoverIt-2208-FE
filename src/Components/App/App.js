ceb292d2543d37f022e0199b47f9fae577390ce9
Dani Bagley <bagley.a.danielle@gmail.com>
2023-02-10 09:41:12 -0700

    Add graph ql package

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,18 +7,48 @@ import SavedPlaces from "../SavedPlaces/SavedPlaces";
 import PlaceDetails from "../PlaceDetails/PlaceDetails";
 import Dashboard from "../Dashboard/Dashboard";
 import samplePlaces from "../sampleData/samplePlaces";
+import { useQuery, gql } from "@apollo/client";
 
 const App = () => {
   const [city, setCity] = useState("Denver");
   const [places, setPlaces] = useState(samplePlaces);
-  // const [error, setError] = useState(false);
-  // const [loading, setLoad] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoad] = useState(false);
+
+  const GET_PLACES = gql`
+    query GetLocations {
+      locations {
+        id
+        name
+        description
+        photo
+      }
+    }
+  `;
+
+  function QueryPlace() {
+    const { loading, error, data } = useQuery(GET_PLACES);
+
+    if (loading) console.log("Loading...");
+    if (error) console.log(`Error ${error.message}`);
+    console.log(data);
+
+    return (
+      <div>
+        <p>Some sort of data</p>
+      </div>
+    );
+  }
 
   return (
     <>
       <Routes>
         <Route exact path="/" element={<SplashPage />} />
-        <Route exact path="/search-page" element={<SearchPage />} />
+        <Route
+          exact
+          path="/search-page"
+          element={<SearchPage setCity={setCity} />}
+        />
         <Route exact path="/saved-places" element={<SavedPlaces />} />
         <Route
           exact
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './Components/App/App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./Components/App/App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://flyby-gateway.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
