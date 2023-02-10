8031b07196f51a653761e49ffb1b191cdb727c34
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-02-10 15:58:00 -0600

    Styling the saved cards

--- src/Components/SavedPlaces/SavedPlaces.js
+++ src/Components/SavedPlaces/SavedPlaces.js
@@ -9,9 +9,11 @@ const SavedPlaces = () => {
       <div className='saved-container'>
         <h1 className='saved-title'>Saved Places</h1>
         <div className='saved-places-container'>
-          lkdfjsalkjdfsakljdfsal;kjdfsal;kjdfsalk;jfdsalk;jfdsalk;jfdsalkj
-        </div>
-        
+          <div className='saved-plaace-card'>
+            <img src='https://static01.nyt.com/images/2016/08/19/dining/19DEATH-WEB/19DEATH-WEB-superJumbo.jpg' alt='death and co'>
+            <h3>Denver</h3>
+            <p>Death & Co.</p>
+          </div>
       </div>
     </>
   )
