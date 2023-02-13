d5f0e25fa12f87c63fabcd40d69370fcb082fbc9
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-02-13 09:00:50 -0600

    Styled badURL page and added back to home button

--- src/Components/BadURL/BadURL.js
+++ src/Components/BadURL/BadURL.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import './BadURL.css'
+import './BadURL.scss'
 import { Link } from 'react-router-dom'
 
 const BadURL = () => {
     return (
         <div className='badURL-container'>
-            <h2 className='badURL-title'>You are on the wrong page<p>Please click below to go back home</p></h2>
+            <h2 className='badURL-title'>You are on the wrong page!<p>Please click the button below to go back home.</p></h2>
             <Link to='/'><button className='home-button'>HOME</button></Link>
         </div>
     )
