43b27ef69a361b607f060b0056132e88fb627d5a
Jenn Yacoubian <jyacoubian@gmail.com>
2023-02-13 05:56:28 -0700

    Add tests for splash page

---
+++ cypress/e2e/splash-page.cy.js
@@ -0,0 +1,20 @@
+describe('Splash Page Flows', () => {
+  beforeEach(() => {
+    // cy.intercept('', {
+    //   method: 'GET',
+    //   fixture: '../fixtures/.json'
+    // })
+    cy.visit('http://localhost:3000')
+  })
+
+  it('should display background, logo, and pick a city button upon page load', () => {
+    cy.get('html').should('have.css', 'background').and('include', 'http://localhost:3000/static/media/DiscoverItBackground.9076687e3ac2cae6ea07.png')
+      .get('.splash-logo').should('be.visible')
+      .get('.pick-button').should('be.visible')
+  })
+
+  it('should go to search by city page when user clicks pick a city button', () => {
+    cy.get('.pick-button').click()
+    cy.visit('http://localhost:3000/search-page')
+  })
+})
