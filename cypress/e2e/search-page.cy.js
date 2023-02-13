d0d89286fbe5624937f2853ab66a2274a9bc13bf
Jenn Yacoubian <jyacoubian@gmail.com>
2023-02-13 06:34:25 -0700

    Add tests for live search results and reminder to add navigation for user input

--- cypress/e2e/search-page.cy.js
+++ cypress/e2e/search-page.cy.js
@@ -30,4 +30,18 @@ describe('Search Page User Flows', () => {
       .type('nver')
       .should('have.value', "Denver")
   })
+
+  it('should display live search results as user types', () => {
+    cy.get('[placeholder="Enter City Name..."]')
+      .type('Denver,')
+      .get('.auto-complete-items').should('be.visible')
+      .get('#0.search-result').should('contain', 'Denver, CO, United States of America')
+      .get('#1.search-result').should('contain', 'Denver, IA 50622, United States of America')
+      .get('#2.search-result').should('contain', 'Denver City, TX 79323, United States of America')
+      //does three search results feel like enough to test here?
+  })
+
+  it('should navigate to dashboard for city that user selects upon click', () => {
+    //not yet functional, come back later to add test
+  })
 })
