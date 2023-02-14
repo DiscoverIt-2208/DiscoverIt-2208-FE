38c788b3df1fa62e27cd0dbeb813e2e9dcdd036b
Dani Bagley <bagley.a.danielle@gmail.com>
2023-02-14 15:12:15 -0700

    Add places query

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,30 +1,106 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 import "./Dashboard.scss";
 import PlaceCard from "../PlaceCard/PlaceCard";
 import NavBar from "../NavBar/NavBar";
 import SampleData from "../sampleData/samplePlaces";
+import Death from "../assets/deathandco.jpg";
 
 const Dashboard = ({ city, places, setPlaces }) => {
-  useEffect(() => {
-    setPlaces(SampleData[0].places);
-  }, [places, city]);
+  const [categories, setCategories] = useState([]);
+
+  // useEffect(() => {
+  //   setPlaces(SampleData[0].places);
+  // }, [places, city]);
+
+  const FETCH_PLACES = gql`
+    query FetchPlaces {
+      places(
+        city: "Denver"
+        country: "US"
+        categories: ["production.brewery", "education.library"]
+      ) {
+        name
+        address
+        placeId
+        categories
+        lat
+        lon
+      }
+    }
+  `;
+
+  const DisplayPlaces = () => {
+    const { data, loading, error } = useQuery(FETCH_PLACES);
+
+    if (loading) {
+      console.log("Submitting...");
+      return <p className="error">Submitting...</p>;
+    }
+    if (error) {
+      console.log(`Submission error! ${error.message}`);
+      return <p className="error">Submission error! {error.message}</p>;
+    }
+
+    console.log(data);
+
+    const eachPlace = data.map((place) => {
+      <PlaceCard place={place} city={city.properties.city} />;
+    });
+
+    return (
+      <div className="place-card-box">
+        <div className="place-card">{eachPlace}</div>
+      </div>
+    );
+  };
+
+  const changeCategory = (e) => [setCategories([e.target.value])];
 
   return (
     <>
-      <NavBar city={city} />
-      <h1 className="city-name">{city}</h1>
+      <NavBar city={city.properties.city} />
+      <h1 className="city-name">{city.properties.city}</h1>
       <div className="buttons-container">
-        <button className="category-button">Restaurant</button>
-        <button className="category-button">Entertainment</button>
-        <button className="category-button">History</button>
-        <button className="category-button">Cafe</button>
-        <button className="category-button">Popular</button>
+        <button
+          className="category-button"
+          value={"catering.restaurant"}
+          onClick={changeCategory}
+        >
+          Restaurant
+        </button>
+        <button
+          className="category-button"
+          value={"entertainment"}
+          onClick={changeCategory}
+        >
+          Entertainment
+        </button>
+        <button
+          className="category-button"
+          value={"building.historic"}
+          onClick={changeCategory}
+        >
+          History
+        </button>
+        <button
+          className="category-button"
+          value={"catering.cafe"}
+          onClick={changeCategory}
+        >
+          Cafe
+        </button>
+        <button
+          className="category-button"
+          value={"tourism.attraction, tourism.sights"}
+          onClick={changeCategory}
+        >
+          Popular
+        </button>
+        <button className="category-button">Accessibility</button>
       </div>
-      <div className="place-card-box">
-        <div className="place-card">
-          <PlaceCard places={places} city={city} />
-        </div>
-      </div>
+      <DisplayPlaces />
     </>
   );
 };
--- src/Components/PlaceCard/PlaceCard.js
+++ src/Components/PlaceCard/PlaceCard.js
@@ -2,19 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./PlaceCard.scss";
 
-const PlaceCard = ({ places, city }) => {
-  const eachPlace = places.map((place) => {
-    return (
-      <Link to={`/${city}/${place.id}`} className="place-thumb" key={place.id}>
-        <div id={`${place.id}`} className="card-container" key={place.name}>
-          <img className="card-img" src={place.image} alt={`${place.name}`} />
-          <h3>{place.name}</h3>
-        </div>
-      </Link>
-    );
-  });
+const PlaceCard = ({ place, city }) => {
+  return (
+    <Link to={`/${city}/${place.id}`} className="place-thumb" key={place.id}>
+      <div id={`${place.id}`} className="card-container" key={place.name}>
+        <img className="card-img" src={place.image} alt={`${place.name}`} />
+        <h3>{place.name}</h3>
+      </div>
+    </Link>
+  );
 
-  return <div className="place-card-container">{eachPlace}</div>;
+  //   return <div className="place-card-container">{eachPlace}</div>;
 };
 
 export default PlaceCard;
