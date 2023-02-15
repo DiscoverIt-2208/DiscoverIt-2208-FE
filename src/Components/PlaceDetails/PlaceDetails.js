bcaf1f0a1742b8ce8ecf42a8e06e87cdc285b4de
Dani Bagley <bagley.a.danielle@gmail.com>
2023-02-15 16:46:43 -0700

    add delete functionality

--- src/Components/PlaceDetails/PlaceDetails.js
+++ src/Components/PlaceDetails/PlaceDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { gql, useMutation } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { Link, useParams } from "react-router-dom";
 import "./PlaceDetails.scss";
 import NavBar from "../NavBar/NavBar";
@@ -10,6 +10,54 @@ const PlaceDetails = ({ city }) => {
   const [details, setDetails] = useState({});
 
   const { id } = useParams();
+
+  const FETCH_PLACE = gql`
+    query FetchPlace($id: String!) {
+      name
+      city
+      state
+      country
+      phone
+      website
+      hours
+      categories
+      address
+      lat
+      lon
+    }
+  `;
+
+  const DisplayPlace = () => {
+    const { loading, error, data } = useQuery(FETCH_PLACE, {
+      variables: {
+        id: id,
+      },
+    });
+    if (loading) return <p>"Loading..."</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    console.log(data);
+
+    return (
+      <div className="detailsThumb" alt={details.name}>
+        <h1 className="detailsTitle">{details.name}</h1>
+        <CreateUserFavorite />
+        <div className="detailsInformation">
+          <img
+            className="detailsImage"
+            src={details.image}
+            alt={details.name}
+          />
+          <div className="information">
+            <p className="infoText">Phone: {details.phoneNumber}</p>
+            <p className="infoText">Hours: {details.hours}</p>
+            <p className="infoText">Address: {details.address}</p>
+            <p className="infoText">Description: {details.description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
   const CreateUserFavorite = () => {
     const [createUserFavorite, { data, loading, error }] = useMutation(
@@ -50,10 +98,11 @@ const PlaceDetails = ({ city }) => {
     <>
       <NavBar city={city.properties.city} />
       <div className="detailsPage">
-        <Link to={`/${city.properties.city}/dashboard`} className="backButton">
+        <Link to={`/dashboard`} className="backButton">
           Back
         </Link>
-        <div className="detailsThumb" alt={details.name}>
+        <DisplayPlace />
+        {/* <div className="detailsThumb" alt={details.name}>
           <h1 className="detailsTitle">{details.name}</h1>
           <CreateUserFavorite />
           <div className="detailsInformation">
@@ -69,7 +118,7 @@ const PlaceDetails = ({ city }) => {
               <p className="infoText">Description: {details.description}</p>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
--- src/Components/SavedDetails/SavedDetails.js
+++ src/Components/SavedDetails/SavedDetails.js
@@ -1,53 +1,57 @@
-import React from 'react'
-import './SavedDetails.scss'
+import React from "react";
+import "./SavedDetails.scss";
 import { Link, useParams } from "react-router-dom";
 import { GET_USER } from "../Queries";
 import { DELETE_USER_FAVORITE } from "../Queries";
 import { gql, useMutation } from "@apollo/client";
-import NavBar from '../NavBar/NavBar'
-
+import NavBar from "../NavBar/NavBar";
 
 const SavedDetails = ({ city }) => {
+  const { id } = useParams();
 
-    const { id } = useParams()
+  const DeleteUserFavorite = () => {
+    const [deleteUserFavorite, { loading, error, data }] = useMutation(
+      DELETE_USER_FAVORITE,
+      {
+        variables: {
+          id: +id,
+        },
+        refetchQueries: [{ query: GET_USER }, "GetUser"],
+      }
+    );
 
-    const DeleteUserFavorite = () => {
-        const deleteUserFavorite = useMutation(
-            DELETE_USER_FAVORITE,
-            {
-                variables: {
-                    id: 1
-                },
-                refetchQueries: [{ query: GET_USER }, "GetUser"]
-            }
-        )
+    if (loading) console.log("Loading...");
+    if (error) console.log(`Error: ${error.message}`);
 
-        return (
-            <Link to='/saved-places'>
-                <button 
-                    className='delete-button' 
-                >
-                    Delete
-                </button>
-            </Link>
-        )
-    }
+    console.log(data);
+
+    return (
+      <Link to="/saved-places">
+        <button
+          className="delete-button"
+          onClick={() => {
+            deleteUserFavorite();
+          }}
+        >
+          Delete
+        </button>
+      </Link>
+    );
+  };
 
   return (
     <>
-        <NavBar />
-        <div className='saved-details-page'>
-            <Link to={`/saved-places`} className="backButton">
-            Back
-            </Link>
-            <h1 className='saved-details-title'>Title of place</h1>
-            <div className='saved-details-container'>
-
-            </div>
-            <DeleteUserFavorite />
-        </div>
+      <NavBar />
+      <div className="saved-details-page">
+        <Link to={`/saved-places`} className="backButton">
+          Back
+        </Link>
+        <h1 className="saved-details-title">Title of place</h1>
+        <div className="saved-details-container"></div>
+        <DeleteUserFavorite />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SavedDetails
+export default SavedDetails;
