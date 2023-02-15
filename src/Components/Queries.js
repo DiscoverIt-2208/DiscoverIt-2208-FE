152169c464ce2c9735d4256ef662133e9ec774ff
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-02-15 14:23:41 -0600

    Added delete mutation query

--- src/Components/Queries.js
+++ src/Components/Queries.js
@@ -53,3 +53,17 @@ export const FETCH_PLACES = gql`
     }
   }
 `;
+
+export const DELETE_USER_FAVORITE = gql`
+  mutation DeleteUserFavorite(
+    $id: Int!
+  ) {
+    deleteUserFavorite(
+      input: {
+        id: $id
+      }
+    ) {
+      success
+    }
+  }
+`
