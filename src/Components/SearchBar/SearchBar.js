520457f08f5f8596695bd3273abdbbafc9b34abd
Jenn Yacoubian <jyacoubian@gmail.com>
2023-02-16 07:45:44 -0700

    remove ununsed "result" variable

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -12,7 +12,7 @@ const SearchBar = ({ setCity, city }) => {
         const requestOptions = {
           method: "GET",
         };
-        const response = await fetch(
+        await fetch(
           `https://api.geoapify.com/v1/geocode/autocomplete?lang=en&limit=10&type=city&text=${searchInput}&apiKey=7ea7d5b3e7214f178782e2a2fc4cf79d`,
           requestOptions
         ).then((response) => response.json())
