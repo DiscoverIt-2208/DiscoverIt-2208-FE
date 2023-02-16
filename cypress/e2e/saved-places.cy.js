4f86733df2e7e01f48f6b2041447b96e03be9b9c
Jenn Yacoubian <jyacoubian@gmail.com>
2023-02-15 23:11:42 -0700

    Comment out saved places notes

--- cypress/e2e/saved-places.cy.js
+++ cypress/e2e/saved-places.cy.js
@@ -1,8 +1,8 @@
-describe('Favorites Page User Flow', () => {
-  cy.visit('http://localhost:3000/Denver/saved-places')
-})
+// describe('Favorites Page User Flow', () => {
+//   cy.visit('http://localhost:3000/Denver/saved-places')
+// })
 
-it('should navigate to saved places page if user clicks saved places in nav bar', () => {
-  cy.get('[href="/saved-places"] > h4').click()
-  cy.visit('http://localhost:3000/saved-places')
-})
+// it('should navigate to saved places page if user clicks saved places in nav bar', () => {
+//   cy.get('[href="/saved-places"] > h4').click()
+//   cy.visit('http://localhost:3000/saved-places')
+// })
